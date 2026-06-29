2bf8e982908ba77cfebf492bce24944066f21951
resc20260223-tech <resc.20260223@gmail.com>
2026-06-30 01:54:22 +0900

    Update diagnose.js

--- api/diagnose.js
+++ api/diagnose.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -31,4 +33,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
